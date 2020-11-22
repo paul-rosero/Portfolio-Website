9b59c378159d0c1758ae6b0ba5c845136301f66d
Paul Rosero <paul.rosero23@gmail.com>
2020-11-22 11:50:01 -0500

    change the href from # to / so it doesnt error out

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ class App extends Component  {
         <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
           <Header transparent title="Title" style={{color: 'white'}}>
             <Navigation>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
             </Navigation>
           </Header>
           <Drawer title="Title">
             <Navigation>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
             </Navigation>
           </Drawer>
           <Content />
