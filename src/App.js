79cc94feb570abb22a8f215cd6b9b43ad3d43006
Paul Rosero <paul.rosero23@gmail.com>
2020-11-22 14:14:20 -0500

    add layout for webpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
-
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import './App.css';
 
 class App extends Component  {
   render() {
     return (  
-      <div>
-        
-      </div>
+      <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
     );
   }
 }    
