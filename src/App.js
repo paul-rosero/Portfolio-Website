8bc734076127dde87ba064ddd3d97dde806d6d14
Paul Rosero <paul.rosero23@gmail.com>
2020-11-22 15:23:42 -0500

    rearrange structure so its readable

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ class App extends Component  {
   render() {
     return (  
       <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
+        <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+          <Header transparent title="Title" style={{color: 'white'}}>
             <Navigation>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
             </Navigation>
-        </Header>
-        <Drawer title="Title">
+          </Header>
+          <Drawer title="Title">
             <Navigation>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
             </Navigation>
-        </Drawer>
-        <Content />
-    </Layout>
-</div>
+          </Drawer>
+          <Content />
+        </Layout>
+      </div>
     );
   }
 }    
