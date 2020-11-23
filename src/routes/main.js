87e685dc09807b93eaa239ea5fad291cd6968c50
Paul Rosero <paul.rosero23@gmail.com>
2020-11-23 10:47:07 -0500

    add the remaining routes left

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-import Homepage from '../components/homepage'
-import About from '../components/about'
+import Homepage from '../components/homepage';
+import About from '../components/about';
+import Projects from '../components/projects';
+import Resume from '../components/resume';
 
 function Main() {
     return(
         <Switch>
             <Route exact path="/" component={ Homepage } />
             <Route path="/about" component={ About } />
+            <Route path="/projects" component={ Projects } />
+            <Route path="/resume" component={ Resume } />
         </Switch>
     )
     
