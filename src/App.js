ac67a6417cf3065b12ed45a955545216487de6f7
Paul Rosero <paul.rosero23@gmail.com>
2020-11-23 18:51:00 -0500

    change layout so it looks modern and clean

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Link } from 'react-router-dom';
 import Main from './routes/main';
 import './App.css';
 
 class App extends Component  {
   render() {
     return (  
-      <div className="demo-big-content">
-    <Layout>
-        <Header title="Title" scroll>
-            <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
+        <div className="main-portfolio-page">
+        <Layout>
+            <Header title="My Portfolio Page" style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover', color: 'white'}}>
+                <Navigation>
+                    <Link className="link-name" to="/">Homepage</Link>
+                    <Link className="link-name" to="/about">About</Link>
+                    <Link className="link-name" to="/resume">Resume</Link>
+                    <Link className="link-name" to="/projects">Projects</Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation >
+                    <Link to="/">Homepage</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/projects">Projects</Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content">
+                     <Main/>
+                </div>
+            </Content>
+        </Layout>
 </div>
     );
   }
