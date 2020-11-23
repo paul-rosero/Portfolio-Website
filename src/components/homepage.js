7df1263950367056c202d961ac6a4b409fd52d7f
Paul Rosero <paul.rosero23@gmail.com>
2020-11-23 10:36:40 -0500

    add code for Homepage as a stateless component for now

---
+++ src/components/homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Homepage() {
+    return(
+        <div className="home-greeting">
+            <h1 className="home-heading">Welcome to my Home Page.</h1>
+            <div>
+                <p className="home-introduction">My name is Paul Rosero and I'm a Flatiron Bootcamp Alumni. Creating websites, starting from scratch or renovating already written websites, is something I love to do. As a beginner in the business, this website was created from scratch just so I can showcase my skills and abilities.</p>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Homepage
