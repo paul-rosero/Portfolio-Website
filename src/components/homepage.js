f89261c9e3711f8b0bffd21ee6204e493ec8c40b
Paul Rosero <paul.rosero23@gmail.com>
2020-11-24 14:09:57 -0500

    seperate both titles into 2 h1 tags

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -9,9 +9,10 @@ function Homepage() {
                 <Cell col={12}>
                     <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img"/>
                     <div className="banner-text">
-                        <h1 className="home-heading">Software Engineer/Full Stack Web Developer</h1>
+                        <h1 className="home-heading">Software Engineer</h1>
+                        <h1 className="home-heading">Full Stack Web Developer</h1>
                         <hr/>
-                        <p className="home-introduction">My name is Paul Rosero and I'm a Flatiron Bootcamp Alumni. Creating websites, starting from scratch or renovating already written websites, is something I love to do. As a beginner in the business, this website was created from scratch just so I can showcase my skills and abilities.</p>
+                        <p className="home-introduction">Creating websites, starting from scratch or renovating already written websites, is something I love to do. As a beginner in the business, this website was created from scratch just so I can showcase my skills and abilities.</p>
                         <div className="social-links">
                             {/*Github icon */}
                             <a href="https://github.com/paul-rosero" target="_blank" rel="noreferrer"><i class="fa fa-github-square"/></a>
@@ -19,13 +20,9 @@ function Homepage() {
                             <a href="https://www.linkedin.com/in/paul-rosero" target="_blank" rel="noreferrer"><i class="fa fa-linkedin"/></a>
                         </div>
                     </div>
-                    
                 </Cell>
             </Grid>
-            
-            
         </div>
-        
     )
 }
 
