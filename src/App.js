51c292eaaf5c097910a09a3849f891f366388a0e
Paul Rosero <paul.rosero23@gmail.com>
2020-11-24 14:08:33 -0500

    organize the structure so the indents are correct

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ import Main from './routes/main';
 import './App.css';
 
 class App extends Component  {
-  render() {
-    return (  
-        <div className="main-portfolio-page">
-        <Layout>
-            <Header title="My Portfolio Page" style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover', color: 'white'}}>
-                <Navigation>
-                    <Link className="link-name" to="/">Homepage</Link>
-                    <Link className="link-name" to="/about">About</Link>
-                    <Link className="link-name" to="/resume">Resume</Link>
-                    <Link className="link-name" to="/projects">Projects</Link>
-                </Navigation>
-            </Header>
-            <Drawer title="Title">
-                <Navigation >
-                    <Link to="/">Homepage</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/projects">Projects</Link>
-                </Navigation>
-            </Drawer>
-            <Content>
-                <div className="page-content">
-                     <Main/>
-                </div>
-            </Content>
-        </Layout>
-</div>
-    );
-  }
+    render() {
+        return (  
+            <div className="main-portfolio-page">
+                <Layout fixedTabs>
+                    <Header title="My Portfolio Page" style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover', color: 'white'}}>
+                        <Navigation>
+                            <Link className="link-name" to="/">Homepage</Link>
+                            <Link className="link-name" to="/about">About</Link>
+                            <Link className="link-name" to="/resume">Resume</Link>
+                            <Link className="link-name" to="/projects">Projects</Link>
+                        </Navigation>
+                    </Header>
+                    <Drawer title="Title">
+                        <Navigation >
+                            <Link to="/">Homepage</Link>
+                            <Link to="/about">About</Link>
+                            <Link to="/resume">Resume</Link>
+                            <Link to="/projects">Projects</Link>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className="page-content">
+                            <Main/>
+                        </div>
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
 }    
 
 export default App;
