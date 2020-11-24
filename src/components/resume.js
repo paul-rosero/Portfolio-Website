950174c1115a0c5fefaf216d96ab4f0421fa7aeb
Paul Rosero <paul.rosero23@gmail.com>
2020-11-24 15:14:25 -0500

    add pic, name and job title for left side of grid

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Cell, Grid } from 'react-mdl';
 
 class Resume extends Component {
     render() {
         return(
             <div>
-                <h1>Resume</h1>
+            <Grid className="resume-grid">
+                <Cell col={4}>
+                    <div style={{ textAlign: 'center'}}>
+                    <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img"/>
+                    
+                    </div>
+                    <h2 style={{paddingTop: '1em'}}>Paul Rosero</h2>
+                    <h5>Full Stack Web Developer</h5>
+                    <hr style={{ borderTop: '3px solid black', width: '60%'}} />
+                </Cell>
+                <Cell col={8} className="resume-right-col">Right Side</Cell>
+            </Grid>
             </div>
         )
     }
