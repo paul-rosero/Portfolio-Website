a82b5cf58784ae9c4871737cd7b0c71b832c47ef
Paul Rosero <paul.rosero23@gmail.com>
2020-11-23 19:08:45 -0500

    fix typo and move p tag into banner class div tag

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -14,9 +14,11 @@ function Homepage() {
                         className="avatar-img"
                     />
                     <div className="banner-text">
-                        <h1 className="home-heading">Software Enginner/Full Stack Web Developer</h1>
+                        <h1 className="home-heading">Software Engineer/Full Stack Web Developer</h1>
+                        <hr/>
+                        <p className="home-introduction">My name is Paul Rosero and I'm a Flatiron Bootcamp Alumni. Creating websites, starting from scratch or renovating already written websites, is something I love to do. As a beginner in the business, this website was created from scratch just so I can showcase my skills and abilities.</p>
                     </div>
-                    <p className="home-introduction">My name is Paul Rosero and I'm a Flatiron Bootcamp Alumni. Creating websites, starting from scratch or renovating already written websites, is something I love to do. As a beginner in the business, this website was created from scratch just so I can showcase my skills and abilities.</p>
+                    
                 </Cell>
             </Grid>
             
