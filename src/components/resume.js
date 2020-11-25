de68b0ba29e9c62a4af2791ad7b955ddc4f2b28e
Paul Rosero <paul.rosero23@gmail.com>
2020-11-25 14:33:35 -0500

    change heading and add intro

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Cell, Grid } from 'react-mdl';
 
+
 class Resume extends Component {
     render() {
         return(
@@ -8,13 +9,12 @@ class Resume extends Component {
             <Grid className="resume-grid">
                 <Cell col={4}>
                     <div style={{ textAlign: 'center'}}>
-                    <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img"/>
-                    
+                        <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img"/>
                     </div>
                     <h2 style={{paddingTop: '1em'}}>Paul Rosero</h2>
-                    <h5>Full Stack Web Developer</h5>
+                    <h5>Software Engineer</h5>
                     <hr style={{ borderTop: '3px solid black', width: '60%'}}/>
-                    <p>Info paragraph.</p>
+                    <p>Software Engineer looking for positions as Full Stack, Front-end or Back-end Web developer with knowledge in Ruby and JavaScript, Sinatra, Rails, React, CSS, HTML5, Bootstrap, SASS and Redux, MondoDB and PostgresSQL databases. My work ethic has improved as a result of working in many different fields with different people in varying situations. It has shaped me to be a well-rounded person and I am excited to help people make their lives easier with my apps/webpages/services.</p>
                     <hr style={{ borderTop: '3px solid black', width: '60%'}}/>
                     <h6>Address:</h6>
                     <p>adress.</p>
@@ -24,7 +24,9 @@ class Resume extends Component {
                     <p>paul.rosero23@gmail.com</p>
                     <hr style={{ borderTop: '3px solid black', width: '60%'}}/>
                 </Cell>
-                <Cell col={8} className="resume-right-col">Right Side</Cell>
+                <Cell col={8} className="resume-right-col">
+                    
+                </Cell>
             </Grid>
             </div>
         )
