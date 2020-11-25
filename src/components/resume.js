e47bad54348a30acb4e925967d816418050ec142
Paul Rosero <paul.rosero23@gmail.com>
2020-11-25 15:03:35 -0500

    add phone and address to each place

--- src/components/resume.js
+++ src/components/resume.js
@@ -18,9 +18,9 @@ class Resume extends Component {
                     <p>Software Engineer looking for positions as Full Stack, Front-end or Back-end Web developer with knowledge in Ruby and JavaScript, Sinatra, Rails, React, CSS, HTML5, Bootstrap, SASS and Redux, MondoDB and PostgresSQL databases. My work ethic has improved as a result of working in many different fields with different people in varying situations. It has shaped me to be a well-rounded person and I am excited to help people make their lives easier with my apps/webpages/services.</p>
                     <hr style={{ borderTop: '3px solid black', width: '60%'}}/>
                     <h6>Address:</h6>
-                    <p>adress.</p>
+                    <p>Deer Park, N.Y.</p>
                     <h6>Phone:</h6>
-                    <p>phone</p>
+                    <p>(631)637-5976</p>
                     <h6>Email:</h6>
                     <p>paul.rosero23@gmail.com</p>
                     <hr style={{ borderTop: '3px solid black', width: '60%'}}/>
