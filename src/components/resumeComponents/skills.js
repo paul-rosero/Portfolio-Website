64af9c3f0f5446ced702ffada082130316ae14c4
Paul Rosero <paul.rosero23@gmail.com>
2020-11-25 19:53:20 -0500

    create a class for skills to import

---
+++ src/components/resumeComponents/skills.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Cell, Grid } from 'react-mdl'
+
+export class Skills extends Component {
+    render() {
+        return (
+            <Grid>
+                <Cell>
+                    
+                </Cell>
+            </Grid>
+        )
+    }
+}
+
+export default Skills
