5726d6dc3887e1df8fa6e8bcd0f9c79f72612b87
Paul Rosero <paul.rosero23@gmail.com>
2020-11-27 12:25:58 -0500

    add renderOther to line 70, to render on page

--- src/components/resumeComponents/skills.js
+++ src/components/resumeComponents/skills.js
@@ -60,13 +60,14 @@ class Skills extends Component {
         return (
             <Grid>
                 <Cell col={12}>
-                <h6>Languages:</h6>
-                    { renderLanguage }
-                <h6>Frameworks:</h6>
-                    { renderFramework }
-                <h6>Databases:</h6>
-                    { renderDatabase }
-                <h6>Other:</h6>   
+                    <h6>Languages:</h6>
+                        { renderLanguage }
+                    <h6>Frameworks:</h6>
+                        { renderFramework }
+                    <h6>Databases:</h6>
+                        { renderDatabase }
+                    <h6>Other:</h6>
+                        { renderOther }
                 </Cell>
             </Grid>
         )
