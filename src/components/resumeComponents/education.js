639e473cf374d4009910d511f2649800833d5101
Paul Rosero <paul.rosero23@gmail.com>
2020-11-26 20:57:04 -0500

    change name to education and change variables

--- src/components/resumeComponents/education.js
+++ src/components/resumeComponents/education.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Cell, Grid } from 'react-mdl';
 
-class ResumeDetails extends Component {
+class Education extends Component {
     render() {
         return (
             <Grid>
@@ -9,12 +9,12 @@ class ResumeDetails extends Component {
                     <p>{this.props.startDate} - {this.props.endDate}</p>
                 </Cell>
                 <Cell col={8}>
-                    <h4 style={{marginTop: '0px'}}>{this.props.institutionName}</h4>
-                    <p>{ this.props.institutionDescription}</p>
+                    <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
+                    <p>{ this.props.schoolDescription}</p>
                 </Cell>
             </Grid>
         )
     }
 }
 
-export default ResumeDetails
+export default Education;
