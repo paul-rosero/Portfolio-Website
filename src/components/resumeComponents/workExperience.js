4170412a28166e249d666da407c58209e5899dd5
Paul Rosero <paul.rosero23@gmail.com>
2020-11-26 20:57:57 -0500

    add workExperience and variables to use

---
+++ src/components/resumeComponents/workExperience.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Cell, Grid } from 'react-mdl';
+
+class WorkExperience extends Component {
+    render() {
+        return (
+            <Grid>
+                <Cell col={4}>
+                    <p>{this.props.startDate} - {this.props.endDate}</p>
+                </Cell>
+                <Cell col={8}>
+                    <h4 style={{marginTop: '0px', marginBottom: '0.3px'}}>{this.props.jobName}</h4>
+                    <h5 style={{marginTop: '0px', marginBottom: '0.3px'}}>{this.props.jobTitle}</h5>
+                    <ul>
+                        <li>{ this.props.jobDescription1}</li>
+                        <li>{ this.props.jobDescription2}</li>
+                        <li>{ this.props.jobDescription3}</li>
+                        <li>{ this.props.jobDescription4}</li>
+                    </ul>
+                </Cell>
+            </Grid>
+        )
+    }
+}
+
+export default WorkExperience;
