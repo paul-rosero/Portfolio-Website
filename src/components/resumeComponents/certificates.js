577978cd4a7d85c0ec0195917719b3bb18828210
Paul Rosero <paul.rosero23@gmail.com>
2020-11-28 18:17:46 -0500

    add data to the array

--- src/components/resumeComponents/certificates.js
+++ src/components/resumeComponents/certificates.js
@@ -2,15 +2,15 @@ import { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
 const certificateArray = [
-    {date: "", name: "", school: ""},
-    {date: "", name: "", school: ""},
-    {date: "", name: "", school: ""},
+    {date: "10/08/2020", name: "JavaScript Essential Training 2019", school: "LinkedIn Learning"},
+    {date: "11/04/2020", name: "Learning Node.js", school: "LinkedIn Learning"},
+    {date: "08/26/2020", name: "Software Engineering Virtual Experience 2020", school: "JP Morgan Chase & CO."},
 ]
 
 const renderCertificate = certificateArray.map(certificate => {
     return <Grid>
         <Cell>
-            
+
         </Cell>
     </Grid>
 })
