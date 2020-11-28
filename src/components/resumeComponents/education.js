63c11167636886f82685cbd571bfbd179c5aee13
Paul Rosero <paul.rosero23@gmail.com>
2020-11-28 16:07:13 -0500

    change the variables so it is easier to work with

--- src/components/resumeComponents/education.js
+++ src/components/resumeComponents/education.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import { Cell, Grid } from 'react-mdl';
 
 const educationArray = [
-    {startDate: "", endDate: "", schoolName: "", description: ""},
-    {startDate: "", endDate: "", schoolName: "", description: ""},
-    {startDate: "", endDate: "", schoolName: "", description: ""}
+    { startDate: "", endDate: "", name: "", description: "" },
+    { startDate: "", endDate: "", name: "", description: "" },
+    { startDate: "", endDate: "", name: "", description: "" }
 ]
 
-const renderEducation = educationArray.map(school => {
+const renderEducation = educationArray.map((school, index) => {
     console.log(school)
+    return <Grid id="education-grid" key={ index }>
+        <Cell col={ 4 }>
+            <p>{ school.startDate } - { school.endDate }</p>
+        </Cell>
+        <Cell col={ 8 }>
+            <h4 style={{ marginTop: '0px' }}>{ school.name }</h4>
+            <p>{ school.description }</p>
+        </Cell>
+    </Grid>
 })
 
 class Education extends Component {
     render() {
         return (
-            <Grid id="education-grid">
-                <Cell col={4}>
-                    <p>{this.props.startDate} - {this.props.endDate}</p>
-                </Cell>
-                <Cell col={8}>
-                    <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
-                    <p>{ this.props.schoolDescription}</p>
-                </Cell>
-            </Grid>
+            [renderEducation]
         )
     }
 }
