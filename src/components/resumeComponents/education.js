330b7c0be41194fb36a19bf4c632d6956a113b90
Paul Rosero <paul.rosero23@gmail.com>
2020-11-28 16:18:12 -0500

    change the h4 to h6 tag and make it bold

--- src/components/resumeComponents/education.js
+++ src/components/resumeComponents/education.js
@@ -3,18 +3,19 @@ import { Cell, Grid } from 'react-mdl';
 
 const educationArray = [
     { startDate: "11/14/2018", endDate: "05/27/2020", name: "Flatiron School", degree: "Certificate of Completion Self-Paced Online Software Engineer" },
-    { startDate: "", endDate: "", name: "", degree: "" },
-    { startDate: "", endDate: "", name: "", degree: "" }
+
+    { startDate: "01/2012", endDate: "12/2013", name: "New York College of Health Professions", degree: "Associates in Occupational Studies" },
+    
+    { startDate: "09/2002", endDate: "6/2006", name: "Stony Brook University", degree: "Bachelors in Biology" }
 ]
 
 const renderEducation = educationArray.map((school, index) => {
-    console.log(school)
     return <Grid id="education-grid" key={ index }>
         <Cell col={ 4 }>
             <p>{ school.startDate } - { school.endDate }</p>
         </Cell>
         <Cell col={ 8 }>
-            <h4 style={{ marginTop: '0px' }}>{ school.name }</h4>
+            <h6 style={{ marginTop: '0px', fontWeight: 'bold' }}>{ school.name }</h6>
             <p>{ school.degree }</p>
         </Cell>
     </Grid>
