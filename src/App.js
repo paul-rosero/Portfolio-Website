ae1c86ec77447cfadf3ed16ea6cfb0ee91b8d173
Paul Rosero <paul.rosero23@gmail.com>
2020-11-30 20:13:49 -0500

    add contact me component to app

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component  {
                             <Link className="link-name" to="/about">About</Link>
                             <Link className="link-name" to="/resume">Resume</Link>
                             <Link className="link-name" to="/projects">Projects</Link>
+                            <Link className="link-name" to="/contact">Contact Me</Link>
                         </Navigation>
                     </Header>
                     <Drawer title={<Link style={{color: "grey", textDecoration: 'none'}} to="/">My Portfolio Page</Link>}>
@@ -23,20 +24,22 @@ class App extends Component  {
                             <Link to="/about">About</Link>
                             <Link to="/resume">Resume</Link>
                             <Link to="/projects">Projects</Link>
+                            <Link to="/contact">Contact Me</Link>
                         </Navigation>
                     </Drawer>
-                    <Content style={{
-                        background: '#9CECFB',  /* fallback for old browsers */
-                        background: '-webkit-linear-gradient(to top, #0052D4, #65C7F7, #9CECFB)',  /* Chrome 10-25, Safari 5.1-6 */
-                        background: 'linear-gradient(to top, #0052D4, #65C7F7, #9CECFB)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-                        top: '0',
-                        left: '0',
-                        width: '100%',
-                        height: '100%'
-                    }}>
-                        <div className="page-content">
-                            <Main/>
-                        </div>
+                    <Content 
+                        style={{
+                            background: '#9CECFB',  /* fallback for old browsers */
+                            background: '-webkit-linear-gradient(to top, #0052D4, #65C7F7, #9CECFB)',  /* Chrome 10-25, Safari 5.1-6 */
+                            background: 'linear-gradient(to top, #0052D4, #65C7F7, #9CECFB)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+                            top: '0',
+                            left: '0',
+                            width: '100%',
+                            height: '100%'
+                        }}>
+                        
+                        <Main/>
+                       
                     </Content>
                 </Layout>
             </div>
