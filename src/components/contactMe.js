7e8938f2c4362e6d79d3193a7f94b77f3c0a1223
Paul Rosero <paul.rosero23@gmail.com>
2020-11-30 20:39:40 -0500

    not sure what happened but had to recommit

---
+++ src/components/contactMe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Cell, Grid } from 'react-mdl';
+
+function ContactMe() {
+    return (
+        <Grid>
+            <Cell>
+
+            </Cell>
+            <Cell>
+
+            </Cell>
+        </Grid>
+    )
+}
+
+export default ContactMe;
