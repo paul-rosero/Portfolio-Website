0e3de1ea325e6b620a4a0f553adc288097923500
Paul Rosero <paul.rosero23@gmail.com>
2020-12-01 11:30:30 -0500

    change paddingTop to marginTop and add style to hr tag

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -6,13 +6,14 @@ function ContactMe() {
         <Grid id="contactMe-grid">
             <Cell col={ 6 }>
                 <h2>Paul Rosero</h2>
-                <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img" style={{ paddingTop: "0px" }}/>
+                <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img" style={{ marginTop: "0px" }}/>
                 <p style={{ fontWeight: "bold", margin: "1em" }}>
                     I want to thank you for taking time to visit my page. If you have any questions, ideas or want to collaborate on some work feel free to message me. I have listed my email address, Github and LinkedIn profile pages so you can reach me faster.
                 </p>
             </Cell>
             <Cell col={ 6 }>
                 <h3>Contact Me</h3>
+                <hr style={{ borderTop: "5px dotted white", width: "75%", margin: "auto" }}/>
                 <List>
                     <ListItem>
                         <ListItemContent></ListItemContent>
