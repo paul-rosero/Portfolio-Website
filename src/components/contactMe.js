ee1b07f6d78fd84ef36f42bc47d4e937ede74fd1
Paul Rosero <paul.rosero23@gmail.com>
2020-12-01 17:17:36 -0500

    change the margin, add padding to p tag in line 10

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -7,7 +7,7 @@ function ContactMe() {
             <Cell col={ 6 }>
                 <h2>Paul Rosero</h2>
                 <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img" style={{ marginTop: "0px" }}/>
-                <p style={{ fontWeight: "bold", margin: "1em" }}>
+                <p style={{ fontWeight: "bold", margin: "1em 2em 2em 2em",padding: "1em" }}>
                     I want to thank you for taking time to visit my page. If you have any questions, ideas or want to collaborate on some work feel free to message me. I have listed my email address, Github and LinkedIn profile pages so you can reach me faster.
                 </p>
             </Cell>
