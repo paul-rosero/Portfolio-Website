d5eab5439175a815b0a2d97d5366ff93bdee1856
Paul Rosero <paul.rosero23@gmail.com>
2020-12-02 12:44:32 -0500

    add code that will show temporary tab names to see it work

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { Tabs, Tab } from 'react-mdl';
 class Projects extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { activeTab: 0 };
+    }
+
     render() {
-        return(
-            <div>
-                <h1>Projects</h1>
-            </div>
-        )
+        return (
+            <div className="projects-category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>Starks</Tab>
+                    <Tab>Lannisters</Tab>
+                    <Tab>Targaryens</Tab>
+                </Tabs>
+                <section>
+                    <div className="content">
+                        Content for the tab: {this.state.activeTab}
+                    </div>
+                </section>
+            </div>    
+        );
     }
 }
 
