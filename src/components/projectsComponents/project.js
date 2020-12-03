46bde0c16306afcf68225d7abcb39f4cb265fe59
Paul Rosero <paul.rosero23@gmail.com>
2020-12-02 20:15:14 -0500

    change width to 75% and height to 500px

--- src/components/projectsComponents/project.js
+++ src/components/projectsComponents/project.js
@@ -1,49 +1,52 @@
 import React from 'react';
-import { Button, Card, CardActions, CardMenu, CardText, CardTitle, IconButton } from 'react-mdl';
+import { Button, Card, CardActions, CardMenu, CardText, CardTitle } from 'react-mdl';
 
 function toggleTabs(activeTab) {
     if (activeTab === 0) {
         return (
-            <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Starks</CardTitle>
+            <Card shadow={ 0 } style={{ width: '75%', margin: 'auto' }}>
+                <CardTitle style={{ color: '#fff', height: '500px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Starks</CardTitle>
                 <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Mauris sagittis pellentesque lacus eleifend lacinia...
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...
                 </CardText>
                 <CardActions border>
                     <Button colored>Get Started</Button>
                 </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name="share"/>
+                <CardMenu style={{ color: '#fff' }}>
+                    <i className="fas fa-share-alt"/>
                 </CardMenu>
-            </Card>)
+            </Card>
+        )
     } else if (activeTab === 1) {
         return (
-            <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Lannisters</CardTitle>
+            <Card shadow={ 0 } style={{ width: '75%', margin: 'auto' }}>
+                <CardTitle style={{ color: '#fff', height: '500px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Lannisters</CardTitle>
                 <CardText>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.Mauris sagittis pellentesque lacus eleifend lacinia...
                 </CardText>
                 <CardActions border>
                     <Button colored>Get Started</Button>
                 </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name="share"/>
+                <CardMenu style={{ color: '#fff' }}>
+                    <i className="fas fa-share-alt"/>
                 </CardMenu>
-            </Card>)
+            </Card>
+        )
     } else if (activeTab === 2) {
         return (
-            <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Targaryens</CardTitle>
+            <Card shadow={ 0 } style={{ width: '75%', margin: 'auto' }}>
+                <CardTitle style={{ color: '#fff', height: '500px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Targaryens</CardTitle>
                 <CardText>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.Mauris sagittis pellentesque lacus eleifend lacinia...
                 </CardText>
                 <CardActions border>
                     <Button colored>Get Started</Button>
                 </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name="share"/>
+                <CardMenu style={{ color: '#fff' }}>
+                    <i className="fas fa-share-alt"/>
                 </CardMenu>
-            </Card>)
+            </Card>
+        )
     }
 }
 
@@ -52,8 +55,6 @@ export default function Project(props) {
         <div>
             { toggleTabs(props.activeTab) }
         </div>
-        
-        
     )
 }
 
