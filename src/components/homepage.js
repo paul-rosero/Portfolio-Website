1bc179705873ca815aab14f597931e4ee4bc3eef
Paul Rosero <paul.rosero23@gmail.com>
2020-12-29 17:08:14 -0500

    change code to have it function without react-mdl

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Grid, Cell } from "react-mdl";
+import { Col, Row } from 'react-bootstrap';
 
 
 function Homepage() {
     return(
         <div className="homepage-greeting" style={{ width: '100%', margin: 'auto'}}>
-            <Grid className="homepage-grid">
-                <Cell col={12}>
+            <Col className="homepage-grid">
+                <Row col={12}>
                     <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img"/>
                     <div className="banner-text">
                         <h1 className="home-heading">Software Engineer</h1>
@@ -28,15 +28,15 @@ function Homepage() {
                         </div>
                         <hr/>
                         <p className="home-introduction">Creating websites, starting from scratch or renovating already written websites, is something I love to do. As a beginner in the business, this website was created from scratch just so I can showcase my skills and abilities.</p>
-                        <div className="social-links" style={{ color: "white" }}>
+                        <div className="social-links">
                             {/*Github icon */}
-                            <a href="https://github.com/paul-rosero" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square"/></a>
+                            <a href="https://github.com/paul-rosero" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square" style={{ color: "white" }}/></a>
                             {/*LinkedIn icon */}
-                            <a href="https://www.linkedin.com/in/paul-rosero" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"/></a>
+                            <a href="https://www.linkedin.com/in/paul-rosero" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin" style={{ color: "white" }}/></a>
                         </div>
                     </div>
-                </Cell>
-            </Grid>
+                </Row>
+            </Col>
         </div>
     )
 }
