7e013e67bf28589158445fe6d60929e9152ccf5d
Paul Rosero <paul.rosero23@gmail.com>
2020-12-29 21:17:20 -0500

    refactor code so its not repetive i tags

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Col, Row } from 'react-bootstrap';
 
+const allTags = [
+    {id: 1, name: "fab fa-js-square"},
+    {id: 2, name: "fab fa-react"},
+    {id: 3, name: "fas fa-gem"},
+    {id: 4, name: "fab fa-html5"},
+    {id: 5, name: "fab fa-bootstrap"},
+    {id: 6, name: "fab fa-mdb"}
+]
+
+const renderTags = allTags.map(({ id, name }) => {
+    return <i key={ id } className={ name }></i>
+})
 
 function Homepage() {
     return(
-        <div className="homepage-greeting" style={{ width: '100%', margin: 'auto'}}>
+        <div className="homepage-greeting">
             <Col className="homepage-grid">
-                <Row col={12}>
+                <Row >
                     <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img"/>
                     <div className="banner-text">
                         <h1 className="home-heading">Software Engineer</h1>
                         <h1 className="home-heading">Full Stack Web Developer</h1>
                         <hr/>
                         <div className="social-links">
-                            {/*JavaScript icon */}
-                            <i className="fab fa-js-square"></i>
-                            {/*React.js icon */}
-                            <i className="fab fa-react"></i>
-                            {/*Ruby icon */}
-                            <i className="fas fa-gem"></i>
-                            {/*HTML5 icon */}
-                            <i className="fab fa-html5"></i>
-                            {/*Bootstrap icon */}
-                            <i className="fab fa-bootstrap"></i>
-                            {/*MongoDB icon */}
-                            <i className="fab fa-mdb"></i>
+                            { renderTags }
                         </div>
                         <hr/>
                         <p className="home-introduction">Creating websites, starting from scratch or renovating already written websites, is something I love to do. As a beginner in the business, this website was created from scratch just so I can showcase my skills and abilities.</p>
