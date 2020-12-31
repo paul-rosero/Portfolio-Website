6ed5f234f821a38bca37ab6ffa5e97a64f8916ef
Paul Rosero <paul.rosero23@gmail.com>
2020-12-31 14:17:58 -0500

    add Container, Row and Col to style it differently

--- src/components/resumeComponents/education.js
+++ src/components/resumeComponents/education.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import { Cell, Grid } from 'react-mdl';
+import { Col, Row, Container } from 'react-bootstrap';
 
 const educationArray = [
     { startDate: "01/2012", endDate: "12/2013", name: "New York College of Health Professions", degree: "Associates Degree in Occupational Studies" },
-
     { startDate: "09/2002", endDate: "6/2006", name: "Stony Brook University", degree: "Bachelor's Degree in Biology" }
 ]
 
-const renderEducation = educationArray.map((school, index) => {
-    return <Grid id="education-grid" key={ index }>
-        <Cell col={ 4 }>
-            <p>{ school.startDate } - { school.endDate }</p>
-        </Cell>
-        <Cell col={ 8 }>
-            <h6 style={{ marginTop: '0px', fontWeight: 'bold' }}>{ school.name }</h6>
-            <p>{ school.degree }</p>
-        </Cell>
-    </Grid>
+const renderEducation = educationArray.map( (school, index) => {
+    return(
+        <Row id="education-grid" key={ index }>
+            <Col col={ 4 }>
+                <p>{ school.startDate } - { school.endDate }</p>
+            </Col>
+            <Col col={ 8 }>
+                <h6 style={{ marginTop: '0px', fontWeight: 'bold' }}>{ school.name }</h6>
+                <p>{ school.degree }</p>
+            </Col>
+        </Row>
+    );
 })
 
 class Education extends Component {
     render() {
         return (
-            [renderEducation]
+            <Container>
+                <h5>Education</h5>
+                { renderEducation }
+            </Container>
+           
         )
     }
 }
