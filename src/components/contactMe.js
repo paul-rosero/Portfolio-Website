2823acc03d7c9d4b0bfa0833f02d9369f8ca796d
Paul Rosero <paul.rosero23@gmail.com>
2021-01-01 11:30:35 -0500

    add css to display page as needed

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Container } from 'react-bootstrap';
 
 const allIcons = [
     { iTag: "fab fa-twitter-square", a: "https://twitter.com/paulrosero33", title: "My Twitter" },
@@ -13,29 +13,31 @@ const renderIcons = allIcons.map( ({ iTag, a, title }, index) => {
     return(
         <li id="icon" key={ index } >
             <i className={ iTag }/>
-                <a href={ a } target="_blank" rel="noopener noreferrer" style={{ fontWeight: 'bold'}}>{title}</a>
+            <a href={ a } target="_blank" rel="noopener noreferrer" style={{ fontWeight: 'bold', color: "white", padding: "0px 0px 0px 10px" }}>{title}</a>
         </li>
     );
 })                         
                         
 function ContactMe() {
     return (
-        <Col id="contactMe-grid">
-            <Row col={ 6 }>
-                <h2>Paul Rosero</h2>
-                <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img" style={{ marginTop: "0px" }}/>
-                <p style={{ fontWeight: "bold", margin: "1em 2em 2em 2em",padding: "1em" }}>
-                    I want to thank you for taking time to visit my page. If you have any questions, ideas or want to collaborate on some work feel free to message me. I have listed my email address, Github and LinkedIn profile pages so you can reach me faster.
-                </p>
+        <Container>
+            <Row className="contactMe-grid">
+                <Col>
+                    <h2>Paul Rosero</h2>
+                    <img src="/images/ProfilePicture.png" alt="avatar" className="avatar-img"/>
+                    <p style={{ fontWeight: "bold", padding: "1em" }}>
+                        I want to thank you for taking time to visit my page. If you have any questions, ideas or want to collaborate on some work feel free to message me. I have listed my email address, Github and LinkedIn profile pages so you can reach me faster.
+                    </p>
+                </Col>
+                <Col>
+                    <h3>Contact Me</h3>
+                    <hr style={{ borderTop: "5px dotted white", width: "85%", margin: "auto" }}/>
+                    <ul className="list-unstyled" style={{ contentAlign: "left" }}>
+                        { renderIcons }
+                    </ul>
+                </Col>
             </Row>
-            <Row col={ 6 }>
-                <h3>Contact Me</h3>
-                <hr style={{ borderTop: "5px dotted white", width: "85%", margin: "auto" }}/>
-                <ul className="list-unstyled">
-                    { renderIcons }
-                </ul>
-            </Row>
-        </Col>
+        </Container>
     )
 }
 
