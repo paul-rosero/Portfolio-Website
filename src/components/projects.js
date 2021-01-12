8ad29b53552fc9d6d5a0ccaca99878bfb8a8b414
Paul Rosero <paul.rosero23@gmail.com>
2021-01-12 13:20:08 -0500

    add code that fetch the github api for my repos

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
 import Project from './projectsComponents/project';
 
-// function ControlledTabs() {
-//     const [key, setKey] = useState('home');
-//     return (
-//         <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)}>
-//             <Tab eventKey="home" title="Home">
-//                 <Sonnet/>
-//             </Tab>
+const repos = fetch("https://api.github.com/users/paul-rosero/starred")
+.then(promise => promise.json())
+.then(data => { return data })
 
-//             <Tab eventKey="profile" title="Profile">
-//                 <Sonnet/>
-//             </Tab>
+console.log(repos)
+function ControlledTabs() {
+    const [key, setKey] = useState('home');
+    return (
+        <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)}>
+            <Tab eventKey="home" title="Home">
+                <Project repository={ repos } />
+            </Tab>
 
-//             <Tab eventKey="contact" title="Contact" disabled>
-//                 <Sonnet/>
-//             </Tab>
-//         </Tabs>
-//     );
-// }
+            <Tab eventKey="profile" title="Profile">
+                
+            </Tab>
+
+            <Tab eventKey="contact" title="Contact" disabled>
+               
+            </Tab>
+        </Tabs>
+    );
+}
     
 class Projects extends Component {
     
@@ -27,7 +32,7 @@ class Projects extends Component {
         return (
             <div>
                 <h1>Under Construction, come back soon to check it out.</h1>
-                {/* <ControlledTabs /> */}
+                <ControlledTabs />
             </div>    
         );
     }
