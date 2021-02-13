bf7764b7d548af4a4e1acc4dfb8c276c2128c08a
Paul Rosero <paul.rosero23@gmail.com>
2021-02-13 16:57:14 -0500

    refactor code to make it work more efficiently

--- src/components/projectsComponents/project.js
+++ src/components/projectsComponents/project.js
@@ -1,33 +1,50 @@
 import React from 'react';
-import { CardDeck, Card } from 'react-bootstrap';
+import { CardDeck, Card, Row, Col } from 'react-bootstrap';
+import moment from 'moment';
+
+const setProfilePic = (name) => {
+    const myProfilePic = '/images/myProfilePage.png';
+    const wysProfilePic = '/images/marisolProfile.png';
+    if (name === "Whats-Your-Story-Website") {
+        return <Card.Img id="project-avatar" variant="top" src={ wysProfilePic }/>;
+    } else if (name === "Portfolio-Website") {
+        return <Card.Img id="project-avatar" variant="top" src={ myProfilePic }/>;
+    } else {
+        return null;
+    }
+}        
 
 const Project = ({ projects }) => {
-    const project = projects.map( ({ name, language, description, homepage, html_url, updated_at }, i) => { 
-    console.log("updated_at", updated_at)      
+    const project = projects.map( ({ name, language, description, homepage, html_url, updated_at }, i) => {
         return (
             homepage ?
-                <Card className="text-center" key={ i }>
-                    <Card.Img variant="top" src="holder.js/100px160"/>
-                    <Card.Body>
-                        <Card.Title>{ name.split("-").join(" ") }</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">{ language }</Card.Subtitle>
-                        <Card.Text>{ description }</Card.Text>
-                    </Card.Body>
-                    <Card.Body>
-                        <Card.Link href={ homepage }>Webpage Link</Card.Link>
-                        <Card.Link href={ html_url }>GitHub Link</Card.Link>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Time now - updated_at = last updated time.</small>
-                    </Card.Footer>
-                </Card>
+                <Col key={ i }>
+                    <Card id="project-card" className="text-center" border="primary" style={{ width: '40vmax' }}>
+                    { setProfilePic(name)}
+                        <Card.Body>
+                            <Card.Title>{ name.split("-").join(" ") }</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">{ language }</Card.Subtitle>
+                            <Card.Text>{ description }</Card.Text>
+                        </Card.Body>
+                        <Card.Body>
+                            <Card.Link href={ homepage }>Webpage Link</Card.Link>
+                            <Card.Link href={ html_url }>GitHub Link</Card.Link>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Updated { moment(updated_at).fromNow() }</small>
+                        </Card.Footer>
+                    </Card>
+                </Col>
             : null  
         )     
     })  
 
     return (
-        <CardDeck>
-           { project }
+        <CardDeck className="container" id="projects-deck">
+            <Row xs={ 1 } md={ 2 } lg={ 2 }>
+                { project }
+            </Row>
+           
         </CardDeck>
     )
 }
